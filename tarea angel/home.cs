430e12fe318d36f852e2ffe8d54da57d50b53916
AprendizBR45 <129765156+AprendizBR45@users.noreply.github.com>
2023-11-05 16:13:05 -0400

    Add files via upload

---
+++ tarea angel/home.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Angel Suarez 
+{
+    class Program
+    {
+        static void Main (string [] args)
+        {
+            console.WriteLine("Angel Suarez");
+        }
+    }
+}
